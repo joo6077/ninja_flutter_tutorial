294dcad0921a06cf0866adbe9c855f362acc40c2
주딘 <joo6077@gmail.com>
2021-04-19 20:23:13 +0900

    중괄호 this 시 생성자 자동 셋팅

--- lib/quote.dart
+++ lib/quote.dart
@@ -2,7 +2,13 @@ class Quote {
   String text;
   String author;
 
+  // this 자동 맵핑
   Quote({this.text, this.author});
+
+  //! Quote({String text, String author}) {
+  //!   this.text = text;
+  //!   this.author = author;
+  //! }
 }
 
 Quote myQuote = Quote(text: 'this is the quote text', author: 'oscar wilde');
