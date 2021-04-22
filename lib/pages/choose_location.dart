17af674b41f5dc6b7715bc84b84dfdb883b7140f
주딘 <joo6077@gmail.com>
2021-04-22 19:46:33 +0900

    routing, initState, buildState

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+
+  void getData() {
+// simulate network request for a username
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function ran');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build function ran');
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            counter += 1;
+          });
+        },
+        child: Text('counter is $counter'),
+      ),
+    );
+  }
+}
