25857a8f480078a573feff37f970eb44a01c5a99
주딘 <joo6077@gmail.com>
2021-04-22 19:47:06 +0900

    last commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,13 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
+import 'package:ninja_flutter_tutorial/pages/choose_location.dart';
+import 'package:ninja_flutter_tutorial/pages/home.dart';
+import 'package:ninja_flutter_tutorial/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-        home: MaterialApp(
-      home: QuoteList(),
-    )));
-
-class QuoteList extends StatefulWidget {
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-// 깃 푸쉬가 안되네
-class _QuoteListState extends State<QuoteList> {
-  List<Quote> quotes = [
-    Quote(
-        author: 'Osca Wilde',
-        text: 'Be yourself; everyone else is already taken'),
-    Quote(
-        author: 'Osca Wilde',
-        text: 'I have nothing to declare except my genius'),
-    Quote(
-        author: 'Osca Wilde',
-        text: 'The truth is rarely pure and never simple'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('AWESOME QUOTES'),
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        children: quotes
-            .map((quote) => QuoteCard(
-                quote: quote,
-                delete: () {
-                  setState(() {
-                    quotes.remove(quote);
-                  });
-                }))
-            .toList(),
-      ),
-    );
-  }
-}
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      },
+    ));
---
+++ lib/practice.dart
@@ -0,0 +1,4 @@
+void main() {
+  Map student = {'name': 'kaido', 'age': 25};
+  print(student['name']);
+}
