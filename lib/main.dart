4f3d8c895a7202619c693d417f3e7d6495b1f462
김형주 <joo6077@gmail.com>
2021-04-27 20:57:11 +0900

    I don't know

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,113 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
 
-void main() => runApp(MaterialApp(
-        home: MaterialApp(
-      home: QuoteList(),
-    )));
+void main() => runApp(MaterialApp(home: NinjaCard()));
 
-class QuoteList extends StatefulWidget {
+class NinjaCard extends StatefulWidget {
   @override
-  _QuoteListState createState() => _QuoteListState();
+  _NinjaCardState createState() => _NinjaCardState();
 }
 
-// 깃 푸쉬
-class _QuoteListState extends State<QuoteList> {
-  List<Quote> quotes = [
-    Quote(
-        author: 'Osca Wilde',
-        text: 'Be yourself; everyone else is already taken'),
-    Quote(
-        author: 'Osca Wilde',
-        text: 'I have nothing to declare except my genius'),
-    Quote(
-        author: 'Osca Wilde',
-        text: 'The truth is rarely pure and never simple'),
-  ];
+class _NinjaCardState extends State<NinjaCard> {
+  int kaidoLevel = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('AWESOME QUOTES'),
-        backgroundColor: Colors.redAccent,
+        title: Text('Ninja ID Card'),
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
-      body: Column(
-        children: quotes
-            .map((quote) => Text('${quote.text}  - ${quote.author}'))
-            .toList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            kaidoLevel += 1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[850],
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/kaido.jpeg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 90.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'Name',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Chun-Li',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '$kaidoLevel',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'chun.li@theninja.co.uk',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
