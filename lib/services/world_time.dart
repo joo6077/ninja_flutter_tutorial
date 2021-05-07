d79b3e706e0c8053e7eb3a99c36a94d7f9f41568
주딘 <joo6077@gmail.com>
2021-05-07 22:11:02 +0900

    loading -> time

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -8,6 +8,30 @@ class WorldTime {
   String flag; // url to an asset flag icon
   String url; // location url for api endpoint
 
-  
-  
+  WorldTime({this.location, this.flag, this.url});
+
+  Future<void> getTime() async {
+    Map<String, String> headers = {
+      'Content-Type': 'application/json;charset=UTF-8',
+      'Charset': 'utf-8'
+    };
+    Response response =
+        await get('https://worldtimeapi.org/api/timezone/Asia/Seoul');
+    print(response.statusCode);
+    Map data = jsonDecode(response.body);
+    print(data);
+
+    // get properties form data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    // print(datetime);
+    // print(offset);
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    // set the time property
+    time = now.toString();
+  }
 }
