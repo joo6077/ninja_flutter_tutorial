5f699c443021e07e96407b146b8cbfc2c65678da
김형주 <joo6077@gmail.com>
2021-05-08 16:49:58 +0900

    caught error

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,4 +1,4 @@
-import 'dart:convert';
+aimport 'dart:convert';
 import 'package:http/http.dart';
 import 'dart:convert';
 
@@ -11,27 +11,32 @@ class WorldTime {
   WorldTime({this.location, this.flag, this.url});
 
   Future<void> getTime() async {
-    Map<String, String> headers = {
-      'Content-Type': 'application/json;charset=UTF-8',
-      'Charset': 'utf-8'
-    };
-    Response response =
-        await get('https://worldtimeapi.org/api/timezone/Asia/Seoul');
-    print(response.statusCode);
-    Map data = jsonDecode(response.body);
-    print(data);
+    try {
+      Map<String, String> headers = {
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Charset': 'utf-8'
+      };
+      Response response =
+          await get('https://worldtimeapi.org/api/timezone/Asia/Seoul');
+      print(response.statusCode);
+      Map data = jsonDecode(response.body);
+      print(data);
 
-    // get properties form data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    // print(datetime);
-    // print(offset);
+      // get properties form data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+      // print(datetime);
+      // print(offset);
 
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      // create DateTime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
 
-    // set the time property
-    time = now.toString();
+      // set the time property
+      time = now.toString();
+ut    } catch (e) {
+      print('caught error: $e');
+      time = 'could not get time data';
+    }
   }
 }
